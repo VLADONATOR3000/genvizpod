17a061f0f5e2732160849c79f4bcc616bade1242
radomirsyper <146690258+radomirsyper@users.noreply.github.com>
2024-06-01 06:36:35 +1000

    update 0.2
    ohhh

--- RABOTA/index.py
+++ RABOTA/index.py
@@ -1,6 +1,18 @@
 # Импортирем нужные библиотеки
 from PIL import Image, ImageDraw, ImageFont
 
+# Открываем изображение 1
+image = Image.open("slide (1).jpg")
+draw = ImageDraw.Draw(image)
+x1, y1 = 50, 100  # Начальная точка
+x2, y2 = 700, 350  # Конечная точка
+draw.rectangle([x1, y1, x2, y2], fill="rgb(234, 243, 250)")
+image.save("RABOTA/img(1_1).jpg")
+
+
+
+
+
 def add_text_to_image(image_path, text, output_path, font_path=None, text_x=None, text_y=None):
     # Открываем изображение
     image = Image.open(image_path)
@@ -30,11 +42,29 @@ def add_text_to_image(image_path, text, output_path, font_path=None, text_x=None
     image.save(output_path)
 
 # Пример использования
-image_path = "slide (3).JPG"
-text = "цфвцфвцфвфцвфцвфцвфцвв"
-output_path = "RABOTA/img.jpg"
 font_path = "arial.ttf"  # путь к файлу шрифта, если используется другой шрифт
-text_x = 60  # координата X текста
-text_y = 170  # координата Y текста
+text = "Текст"
 
-add_text_to_image(image_path, text, output_path, font_path, text_x, text_y)
+output_path = "RABOTA/img(1).jpg"
+image_path = "slide (1).JPG"
+text_x = 60 
+text_y = 120 
+add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для первого фото.
+
+output_path = "RABOTA/img(2).jpg"
+image_path = "slide (2).JPG"
+text_x = 60 
+text_y = 185
+add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для второго фото.
+
+output_path = "RABOTA/img(3).jpg"
+image_path = "slide (3).JPG"
+text_x = 60 
+text_y = 150 
+add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для третьего фото.
+
+output_path = "RABOTA/img(4).jpg"
+image_path = "slide (4).JPG"
+text_x = 60 
+text_y = 150 
+add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для четвертого фото.
