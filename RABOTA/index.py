0959b8960b935538f9e8a09472689eeaa3321d9c
radomirsyper <146690258+radomirsyper@users.noreply.github.com>
2024-06-01 08:48:29 +1000

    update 0.4
    uwu

--- RABOTA/index.py
+++ RABOTA/index.py
@@ -1,4 +1,6 @@
 # Импортирем нужные библиотеки
+import os
+import random
 from PIL import Image, ImageDraw, ImageFont
 
 # Открываем изображение 1
@@ -44,7 +46,7 @@ def add_text_to_image(image_path, text, output_path, font_path=None, text_x=None
 
     # Устанавливаем шрифт
     if font_path:
-        font = ImageFont.truetype(font_path, size=50)
+        font = ImageFont.truetype(font_path, size=40)
     else:
         font = ImageFont.load_default()
 
@@ -67,28 +69,114 @@ def add_text_to_image(image_path, text, output_path, font_path=None, text_x=None
 
 # Пример использования
 font_path = "arial.ttf"  # путь к файлу шрифта, если используется другой шрифт
-text = "Текст"
+text_x = 60 
+text_y = 150
+
+def drop_part_with_space(text):
+    if len(text) > 30:
+        first_space = text.find(' ')
+        if first_space != -1:
+            second_space = text.find(' ', first_space + 1)
+            if second_space != -1 and second_space <= 30:
+                dropped_text = text[:second_space] + '\n' + text[second_space + 1:]
+            else:
+                dropped_text = text[:first_space] + '\n' + text[first_space + 1:]
+        else:
+            dropped_text = text
+    else:
+        dropped_text = text
+    return dropped_text
+
+text = "Пресс-конференция по итогам заседания Совета директоров по денежно-кредитной политике"
+dropped_text = drop_part_with_space(text)
+text = dropped_text 
+
 
 output_path = "RABOTA/img(1).jpg"
 image_path = "RABOTA/img(1_1).JPG"
-text_x = 60 
-text_y = 150 
 add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для первого фото.
 
 output_path = "RABOTA/img(2).jpg"
 image_path = "RABOTA/img(2_1).JPG"
-text_x = 60 
-text_y = 150 
 add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для второго фото.
 
 output_path = "RABOTA/img(3).jpg"
-image_path = "RABOTA/img(3_1).JPG"
-text_x = 60 
-text_y = 150 
+image_path = "RABOTA/img(3_1).JPG" 
 add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для третьего фото.
 
 output_path = "RABOTA/img(4).jpg"
 image_path = "RABOTA/img(4_1).JPG"
-text_x = 60 
-text_y = 150 
 add_text_to_image(image_path, text, output_path, font_path, text_x, text_y) # Запуск для четвертого фото.
+
+
+
+
+# Функция для выбора случайного изображения из папок
+def get_random_image_path(root_dir):
+    # Список путей к изображениям
+    image_paths = []
+
+    # Обход всех файлов и подпапок в корневой папке
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            # Проверка расширения файла
+            if file.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+                image_paths.append(os.path.join(root, file))
+
+    # Возвращаем случайный путь к изображению
+    return random.choice(image_paths)
+
+# Путь к папке с изображениями
+images_folder = "RABOTA/illustrations"
+
+# Получаем случайный путь к изображению
+random_image_path = get_random_image_path(images_folder)
+
+# Открываем случайное изображение
+random_image = Image.open(random_image_path)
+
+# Задаем желаемые параметры размера
+desired_width = 80  # Желаемая ширина
+desired_height = 80  # Желаемая высота
+
+# Получаем текущие размеры изображения
+current_width, current_height = random_image.size
+
+# Задаем желаемые параметры размера
+desired_width = 60  # Желаемая ширина
+desired_height = 60  # Желаемая высота
+
+# Генерируем случайные размеры для уменьшения изображения
+resized_width = random.randint(1, current_width)
+resized_height = random.randint(1, current_height)
+
+# Уменьшаем размеры до максимально допустимых значений
+resized_width = min(resized_width, desired_width)
+resized_height = min(resized_height, desired_height)
+
+# Определяем координаты, куда хотим вставить случайное изображение
+position = (700, 80)  # Пример координат
+
+# Загружаем изображение, в которое хотим вставить случайное изображение
+background_image = Image.open("RABOTA/img(1).jpg")
+
+# Вставляем случайное изображение в фоновое изображение
+background_image.paste(random_image, position)
+
+# Сохраняем результат
+background_image.save("RABOTA/img(1).jpg")
+
+
+background_image = Image.open("RABOTA/img(2).jpg")
+background_image.paste(random_image, position)
+background_image.save("RABOTA/img(2).jpg")
+
+background_image = Image.open("RABOTA/img(3).jpg")
+background_image.paste(random_image, position)
+background_image.save("RABOTA/img(3).jpg")
+
+background_image = Image.open("RABOTA/img(4).jpg")
+background_image.paste(random_image, position)
+background_image.save("RABOTA/img(4).jpg")
+
+
